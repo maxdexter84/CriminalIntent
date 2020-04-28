9f0095f0ff4973796961ef20c1527728f81ebe4f
maxdexter <maxdexter@bk.ru>
2020-04-28 14:35:13 +0300

    Created singleton class CrimeLab

--- app/src/main/java/com/maxdexter/criminalintent/CrimeLab.java
+++ app/src/main/java/com/maxdexter/criminalintent/CrimeLab.java
@@ -1,4 +1,18 @@
 package com.maxdexter.criminalintent;
 
+import android.content.Context;
+
 public class CrimeLab {
+    private static CrimeLab sCrimeLab;
+
+    public CrimeLab(Context context) {
+
+    }
+
+    public static CrimeLab get(Context context){
+        if(sCrimeLab == null){
+            sCrimeLab = new CrimeLab(context);
+        }
+        return sCrimeLab;
+    }
 }
